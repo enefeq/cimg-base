42c47e6ffc26f4623082b5b6130245c2e15fb949
Anton I. Sipos <aisipos@gmail.com>
2024-07-16 14:54:03 -0700

    Add Ubuntu24.04 Dockerfile and update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -5,3 +5,4 @@ docker context create cimg
 docker buildx create --use cimg
 docker buildx build --file 20.04/Dockerfile -t cimg/base:20.04 -t cimg/base:20.04 --platform=linux/amd64,linux/arm64 .
 docker buildx build --file 22.04/Dockerfile -t cimg/base:22.04 -t cimg/base:22.04 --platform=linux/amd64,linux/arm64 .
+docker buildx build --file 24.04/Dockerfile -t enefeq/cimg_base:24.04 --platform=linux/amd64 --load . 
